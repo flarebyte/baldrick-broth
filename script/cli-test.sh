0faf974d91f1543986a23804810af0bbf47d251f
Olivier Huin <olih@users.noreply.github.com>
2022-07-26 09:28:10 +0100

    update cli-test

--- script/cli-test.sh
+++ script/cli-test.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 rm -rf report/shell-tests
 mkdir -p report/shell-tests
-yarn cli object report/shell-tests/dest.json package.json tsconfig.json script/fixture/Example.elm
-if [ ! -f "report/shell-tests/dest.json" ]; then
-    echo "❌ KO dest.json"
-    exit 1
-fi
+yarn cli --help
