014388df1950a80ed7a7097b74ce55833a8fb0c3
Olivier Huin <olih@users.noreply.github.com>
2022-07-27 09:28:37 +0100

    Check for loading yaml

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,19 @@
 import { Command } from 'commander';
 import { createCommands } from './commands-creator.js';
+import { buildFilePath } from './env-variables.js';
+import { readYaml } from './file-io.js';
 import { version } from './version.js';
 
 export async function runClient() {
   try {
+    const buildLoadingStatus = await readYaml(buildFilePath)
+    if (buildLoadingStatus.status === 'parse-yaml-error') {
+      console.error(`The file ${buildLoadingStatus.filename} cannot be parsed as YAML`);
+      process.exit(1)
+    }
+    if (buildLoadingStatus.status === 'success') {
+      console.log(`Successfully loaded the build file`)
+    }
     const program = new Command();
     createCommands(program);
     program.parseAsync();
