bf834e293832fbd4b27bd4f553c59d622bc7292d
Olivier Huin <olih@users.noreply.github.com>
2022-07-28 08:49:38 +0100

    tweaks schema for build-model

--- src/build-model.ts
+++ src/build-model.ts
@@ -10,47 +10,60 @@ const engine = z.object({
   defaultShell: z.string(),
   telemetry: z.object({ verbosity: z.string(), filepath: z.string() }),
 });
+const stringTitle = z.string().trim().min(1).max(60);
+const stringDescription = z.string().trim().min(1).max(300);
+const stringMotivation = z.string().trim().min(1).max(300);
+const stringUrl = z.string().url().max(300);
+const stringShell = z.string().min(1).max(5000);
+const stringShellStepFlag = z.enum(['silent:fail']);
+const stringTaskFlag = z.enum(['private']);
+
 const binary = z.object({
-  description: z.string(),
-  motivation: z.string(),
-  homepage: z.string(),
-  shell: z.object({ run: z.string(), diagnosis: z.string() }),
+  title: stringTitle,
+  description: stringDescription.optional(),
+  motivation: stringMotivation.optional(),
+  homepage: stringUrl.optional(),
+  shell: z.object({ run: stringShell, diagnosis: stringShell }),
+});
+const valuesLoopEach = z.string().min(1).max(300);
+const loopEach = z.object({
+  value: customKey,
+  values: valuesLoopEach,
 });
 const binaries = z.record(customKey, binary);
 const shellStep = z.object({
   shell: z.object({
-    bin: z.string(),
-    title: z.string(),
-    if: z.string(),
-    unless: z.string(),
-    each: z.object({ color: z.string() }),
-    run: z.string(),
-    failSilently: z.boolean(),
+    bin: customKey.optional(),
+    title: stringTitle,
+    if: stringShell.optional(),
+    unless: stringShell.optional(),
+    each: z.array(loopEach).min(1).max(12).optional(),
+    flags: z.set(stringShellStepFlag).optional(),
+    run: stringShell,
   }),
 });
-const taskRef = z.object({ task: z.string() });
+const taskRef = z.object({ task: customKey });
 const steps = z.array(z.union([taskRef, shellStep]));
 const parameter = z.object({ description: z.string() });
 const task = z.object({
-  title: z.string(),
-  description: z.string(),
-  motivation: z.string(),
-  visibility: z.string(),
+  title: stringTitle,
+  description: stringDescription.optional(),
+  motivation: stringMotivation.optional(),
+  flags: z.set(stringTaskFlag).optional(),
   parameters: z.record(customKey, parameter),
   steps,
   finally: steps,
+});
+const domain = z.object({
+  title: stringTitle,
+  description: stringDescription.optional(),
+  tasks: z.record(customKey, task),
 });
 export const schema = z.object({
   engine,
   binaries,
   variables: z.object({}),
-  domains: z.object({
-    test: z.object({
-      title: z.string(),
-      description: z.string(),
-      tasks: z.record(customKey, task),
-    }),
-  }),
+  domains: z.record(customKey, domain),
 });
 
 export const safeParseBuild = (content: unknown) => schema.safeParse(content);
