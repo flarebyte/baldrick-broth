2085679c3c4848690436a5ee27bc0c253e873180
Olivier Huin <olih@users.noreply.github.com>
2022-07-28 08:15:11 +0100

    Restructure build-model

--- src/build-model.ts
+++ src/build-model.ts
@@ -1,71 +1,54 @@
 import { z } from 'zod';
 
+const customKey = z
+  .string()
+  .min(1)
+  .max(30)
+  .regex(/[a-z][a-z0-9_]+/);
+
+const engine = z.object({
+  defaultShell: z.string(),
+  telemetry: z.object({ verbosity: z.string(), filepath: z.string() }),
+});
+const binary = z.object({
+  description: z.string(),
+  motivation: z.string(),
+  homepage: z.string(),
+  shell: z.object({ run: z.string(), diagnosis: z.string() }),
+});
+const binaries = z.record(customKey, binary);
+const shellStep = z.object({
+  shell: z.object({
+    bin: z.string(),
+    title: z.string(),
+    if: z.string(),
+    unless: z.string(),
+    each: z.object({ color: z.string() }),
+    run: z.string(),
+    failSilently: z.boolean(),
+  }),
+});
+const taskRef = z.object({ task: z.string() });
+const steps = z.array(z.union([taskRef, shellStep]));
+const parameter = z.object({ description: z.string() });
+const task = z.object({
+  title: z.string(),
+  description: z.string(),
+  motivation: z.string(),
+  visibility: z.string(),
+  parameters: z.record(customKey, parameter),
+  steps,
+  finally: steps,
+});
 export const schema = z.object({
-  engine: z.object({
-    defaultShell: z.string(),
-    telemetry: z.object({ verbosity: z.string(), filepath: z.string() }),
-  }),
-  binaries: z.object({
-    sh: z.object({
-      description: z.string(),
-      motivation: z.string(),
-      homepage: z.string(),
-      shell: z.object({ run: z.string(), diagnosis: z.string() }),
-    }),
-    elm: z.object({
-      description: z.string(),
-      motivation: z.string(),
-      homepage: z.string(),
-      shell: z.object({ run: z.string(), diagnosis: z.string() }),
-    }),
-    whisker: z.object({
-      description: z.string(),
-      motivation: z.string(),
-      homepage: z.string(),
-      shell: z.object({ run: z.string(), diagnosis: z.string() }),
-    }),
-  }),
-  variables: z.object({
-    githubAccount: z.string(),
-    projectName: z.string(),
-    copyright: z.object({ holder: z.string(), startYear: z.number() }),
-    license: z.string(),
-    project_yaml: z.string(),
-    project_schema: z.string(),
-    generate_sh: z.string(),
-    colors: z.array(z.string()),
-    animals: z.array(z.string()),
-  }),
+  engine,
+  binaries,
+  variables: z.object({}),
   domains: z.object({
     test: z.object({
       title: z.string(),
       description: z.string(),
-      tasks: z.object({
-        generate: z.object({
-          title: z.string(),
-          description: z.string(),
-          motivation: z.string(),
-          visibility: z.string(),
-          parameters: z.object({ only: z.object({ description: z.string() }) }),
-          steps: z.array(
-            z.union([
-              z.object({ task: z.string() }),
-              z.object({
-                shell: z.object({
-                  bin: z.string(),
-                  title: z.string(),
-                  if: z.string(),
-                  unless: z.string(),
-                  each: z.object({ color: z.string() }),
-                  run: z.string(),
-                  failSilently: z.boolean(),
-                }),
-              }),
-            ])
-          ),
-          finally: z.array(z.object({ task: z.string() })),
-        }),
-      }),
+      tasks: z.record(customKey, task),
     }),
   }),
 });
