6f8fa231886d65e7966ac73ee390f63009fe44a9
Olivier Huin <olih@users.noreply.github.com>
2022-07-29 09:14:50 +0100

    test build model

--- src/build-model.ts
+++ src/build-model.ts
@@ -6,6 +6,13 @@ const customKey = z
   .max(30)
   .regex(/[a-z][a-z0-9_]+/);
 
+const literalSchema = z.union([z.string().min(1), z.number(), z.boolean()]);
+type Literal = z.infer<typeof literalSchema>;
+type Json = Literal | { [key: string]: Json } | Json[];
+const jsonishSchema: z.ZodType<Json> = z.lazy(() =>
+  z.union([literalSchema, z.array(jsonishSchema), z.record(jsonishSchema)])
+);
+
 const engine = z.object({
   defaultShell: z.string(),
   telemetry: z.object({ verbosity: z.string(), filepath: z.string() }),
@@ -31,25 +38,25 @@ const loopEach = z.object({
   values: valuesLoopEach,
 });
 const binaries = z.record(customKey, binary);
-const shellStep = z.object({
-  shell: z.object({
-    bin: customKey.optional(),
-    title: stringTitle,
-    if: stringShell.optional(),
-    unless: stringShell.optional(),
-    each: z.array(loopEach).min(1).max(12).optional(),
-    flags: z.set(stringShellStepFlag).optional(),
-    run: stringShell,
-  }),
+const shellStep = z.strictObject({
+  a: z.literal('shell'),
+  bin: customKey.optional(),
+  title: stringTitle,
+  if: stringShell.optional(),
+  unless: stringShell.optional(),
+  each: z.array(loopEach).min(1).max(12).optional(),
+  flags: z.array(stringShellStepFlag).optional(),
+  run: stringShell,
 });
-const taskRef = z.object({ task: customKey });
-const steps = z.array(z.union([taskRef, shellStep]));
+const taskRef = z.strictObject({ a: z.literal('task'), task: customKey });
+const eitherStep = z.discriminatedUnion('a', [taskRef, shellStep]);
+const steps = z.array(eitherStep).min(1);
 const parameter = z.object({ description: z.string() });
 const task = z.object({
   title: stringTitle,
   description: stringDescription.optional(),
   motivation: stringMotivation.optional(),
-  flags: z.set(stringTaskFlag).optional(),
+  flags: z.array(stringTaskFlag).optional(),
   parameters: z.record(customKey, parameter),
   steps,
   finally: steps,
@@ -59,11 +66,124 @@ const domain = z.object({
   description: stringDescription.optional(),
   tasks: z.record(customKey, task),
 });
-export const schema = z.object({
-  engine,
-  binaries,
-  variables: z.object({}),
-  domains: z.record(customKey, domain),
-});
+export const schema = z
+  .object({
+    engine,
+    binaries,
+    model: jsonishSchema,
+    workflows: z.record(customKey, domain),
+  })
+  .strict();
 
-export const safeParseBuild = (content: unknown) => schema.safeParse(content);
+interface ValidationError {
+  message: string;
+  path: string;
+}
+
+export type BuildModelValidation =
+  | {
+      status: 'valid';
+      value: z.infer<typeof schema>;
+    }
+  | {
+      status: 'invalid';
+      errors: ValidationError[];
+    };
+
+const formatMessage = (issue: z.ZodIssue): ValidationError => {
+  const path = issue.path.join('.');
+  switch (issue.code) {
+    case 'invalid_type':
+      return {
+        path,
+        message: [
+          'The type for the field is invalid',
+          `I would expect ${issue.expected} instead of ${issue.received}`,
+        ].join('; '),
+      };
+    case 'invalid_string':
+      return {
+        path,
+        message: [
+          'The string for the field is invalid',
+          `${issue.message} and ${issue.validation}`,
+        ].join('; '),
+      };
+
+    case 'invalid_enum_value':
+      return {
+        path,
+        message: [
+          'The enum for the field is invalid',
+          `I would expect any of ${issue.options} instead of ${issue.received}`,
+        ].join('; '),
+      };
+
+    case 'invalid_literal':
+      return {
+        path,
+        message: [
+          'The literal for the field is invalid',
+          `I would expect ${issue.expected}`,
+        ].join('; '),
+      };
+
+    case 'invalid_union_discriminator':
+      return {
+        path,
+        message: [
+          'The union discriminator for the object is invalid',
+          `I would expect any of ${issue.options}`,
+        ].join('; '),
+      };
+    case 'invalid_union':
+      return {
+        path,
+        message: [
+          'The union for the field is invalid',
+          `I would check ${issue.unionErrors}`,
+        ].join('; '),
+      };
+    case 'too_big':
+      return {
+        path,
+        message: [
+          `The ${issue.type} for the field is too big`,
+          `I would expect the maximum to be ${issue.maximum}`,
+        ].join('; '),
+      };
+
+    case 'too_small':
+      return {
+        path,
+        message: [
+          `The ${issue.type} for the field is too small`,
+          `I would expect the minimum to be ${issue.minimum}`,
+        ].join('; '),
+      };
+
+    default:
+      return {
+        path,
+        message: [
+          'The type for the field is incorrect',
+          `${issue.message}`,
+        ].join('; '),
+      };
+  }
+};
+
+export const safeParseBuild = (content: unknown): BuildModelValidation => {
+  const result = schema.safeParse(content);
+  if (result.success) {
+    return { status: 'valid', value: result.data };
+  }
+  const {
+    error: { issues },
+  } = result;
+  const errors = issues.map(formatMessage);
+  return {
+    status: 'invalid',
+    errors,
+  };
+};
