f8161abe5a43fdd1e6f93351b5558ea6ecd5b0cc
Olivier Huin <olih@users.noreply.github.com>
2022-08-04 09:29:31 +0100

    Remove check for invalid in test

--- test/build-model.test.ts
+++ test/build-model.test.ts
@@ -4,7 +4,6 @@ import { buildModelExample } from './fixture-build-model.js';
 describe('build-model', () => {
   it('should pass through a valid model', () => {
     const actual = safeParseBuild(buildModelExample);
-    expect(actual.status).toStrictEqual('valid');
     expect(
       actual.status === 'valid' ? actual.value : actual.errors
     ).toMatchObject(buildModelExample);
