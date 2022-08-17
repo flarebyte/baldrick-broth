42e87b09aed10863255dacba6987cdded44e2dbf
Olivier Huin <olih@users.noreply.github.com>
2022-08-17 08:28:37 +0100

    Add green tick in zx code

--- script/cli-test.mjs
+++ script/cli-test.mjs
@@ -3,12 +3,19 @@
  * Run with: zx script/cli-test.mjs
  */
 
+const assertSuccess = (test, message) => {
+    if (test) {
+        echo(`✅ OK: ${message}\n---\n`)
+    } else {
+        echo(`❌ KO: ${message}\n---\n`)
+    }
+}
+
 process.env.BALDRICK_BROTH_BUILD_FILE='script/fixture/.baldrick-broth/dev.yaml'
 await $`rm -rf report/shell-tests`
 await $`mkdir -p report/shell-tests`
 
-await $`yarn cli --help`
-echo('---')
+const helpCli = await $`yarn cli --help`
+assertSuccess(`${helpCli}`.includes('CLI for build automation'), 'broth --help')
 await $`yarn cli test generate --help`
-echo('---')
 await $`yarn cli test generate -c blue`
