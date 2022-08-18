20c8485c9605dc06363deb278807c7c639e14a25
Olivier Huin <olih@users.noreply.github.com>
2022-08-18 06:55:26 +0100

    Run top level action

--- src/create-task-action.ts
+++ src/create-task-action.ts
@@ -1,8 +1,27 @@
+import { Listr } from 'listr2';
 import { Ctx } from './batch-model.js';
+import { BatchStepModel } from './build-model.js';
 
-export const createTaskAction = (ctx: Ctx) => (opts: any) => console.log(
-  'createTaskAction',
-  opts,
-  Object.keys(ctx.build.binaries),
-  ctx.task.title
-);
+const toBatchAction = (batchStep: BatchStepModel) => ({
+  title: batchStep.title || 'Batch Step',
+  task: async (ctx: Ctx): Promise<void> => {
+    console.log(`>> ${ctx.task.title}`);
+  },
+});
+
+const toListActions = (ctx: Ctx): Listr<Ctx> => {
+  const listActions = ctx.task.steps.map(toBatchAction);
+  const tasks = new Listr<Ctx>(listActions);
+  return tasks;
+};
+
+export const createTaskAction = (ctx: Ctx) => async (_opts: any) => {
+  const mainTask = toListActions(ctx);
+
+  try {
+    const context = await mainTask.run(ctx);
+    console.log(JSON.stringify(context.data));
+  } catch (e: any) {
+    console.log('Failure ', e);
+  }
+};
