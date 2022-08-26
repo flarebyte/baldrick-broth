8f31c6c2aa643e4420eb369b5c63083aebb9af50
Olivier Huin <olih@users.noreply.github.com>
2022-08-26 09:25:12 +0100

    boolean ops for basic execution

--- test/basic-execution.test.ts
+++ test/basic-execution.test.ts
@@ -23,12 +23,62 @@ const exampleBatchStep: BatchStepModel = {
       name: 'readProp',
       value: 'runtime.project.name',
     },
+    {
+      a: 'some-truthy',
+      name: 'someTruthy',
+      values: ['runtime.project.name', 'should.not.exist'],
+    },
+    {
+      a: 'some-truthy',
+      name: 'someImpossibleTruthy',
+      values: ['should.not.exist.either', 'should.not.exist'],
+    },
+    {
+      a: 'some-falsy',
+      name: 'someFalsy',
+      values: ['runtime.project.name', 'should.not.exist'],
+    },
+    {
+      a: 'some-falsy',
+      name: 'someImpossibleFalsy',
+      values: ['runtime.project.name', 'runtime.pwd'],
+    },
+    {
+      a: 'every-truthy',
+      name: 'everyTruthy',
+      values: ['runtime.project.name', 'runtime.pwd'],
+    },
+    {
+      a: 'every-falsy',
+      name: 'everyFalsy',
+      values: ['should.not.exist.either', 'should.not.exist'],
+    },
+    {
+      a: 'every-truthy',
+      name: 'everyImpossibleTruthy',
+      values: ['runtime.project.name', 'should.not.exist'],
+    },
+    {
+      a: 'every-falsy',
+      name: 'everyImpossibleFalsy',
+      values: ['runtime.project.name', 'should.not.exist'],
+    },
+    {
+      a: 'not',
+      name: 'notTrue',
+      value: 'runtime.project.name',
+    },
+    {
+      a: 'not',
+      name: 'notFalse',
+      value: 'should.not.exist',
+    },
   ],
   commands: [],
 };
 
 describe('basic-execution', () => {
-  it('should provide', () => {
+  it('should provide basic execution', () => {
     const actual = basicExecution(ctx, exampleBatchStep);
     expect(actual.status).toStrictEqual('success');
     expect(actual.status === 'success' && actual.ctx.data)
@@ -36,6 +86,16 @@ describe('basic-execution', () => {
       Map {
         "generate" => Map {
           "readProp" => "baldrick-broth",
+          "someTruthy" => true,
+          "someImpossibleTruthy" => false,
+          "someFalsy" => true,
+          "someImpossibleFalsy" => false,
+          "everyTruthy" => true,
+          "everyFalsy" => true,
+          "everyImpossibleTruthy" => false,
+          "everyImpossibleFalsy" => false,
+          "notTrue" => false,
+          "notFalse" => true,
         },
       }
     `);
