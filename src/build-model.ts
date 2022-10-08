5f075780a519ae56f93e5b08c3f92a3a8d2891be
Olivier Huin <olih@users.noreply.github.com>
2022-10-08 15:22:23 +0100

    Adds description to zod model

--- src/build-model.ts
+++ src/build-model.ts
@@ -15,19 +15,24 @@ import { formatMessage, ValidationError } from './format-message.js';
 const literalSchema = z.union([z.string().min(1), z.number(), z.boolean()]);
 type Literal = z.infer<typeof literalSchema>;
 type Json = Literal | { [key: string]: Json } | Json[];
-const jsonishSchema: z.ZodType<Json> = z.lazy(() =>
-  z.union([literalSchema, z.array(jsonishSchema), z.record(jsonishSchema)])
-);
+const jsonishSchema: z.ZodType<Json> = z
+  .lazy(() =>
+    z.union([literalSchema, z.array(jsonishSchema), z.record(jsonishSchema)])
+  )
+  .describe('Any JSON document without null values');
 const engine = z
   .object({
     defaultShell: z.string(),
-    telemetry: z.object({
-      name: z.string().min(1).max(40).optional(),
-      verbosity: z.string(),
-      filepath: z.string(),
-    }),
+    telemetry: z
+      .object({
+        name: z.string().min(1).max(40).optional(),
+        verbosity: z.string(),
+        filepath: z.string(),
+      })
+      .describe('Preferences for telemetry'),
   })
-  .optional();
+  .optional()
+  .describe('Settings for the baldrick-broth engine');
 const onBatchStepFinish = z.enum(['exit', 'silent']);
 
 const onShellCommandFinish = z.enum([
@@ -40,171 +45,278 @@ const onShellCommandFinish = z.enum([
 ]);
 
 const metadataStep = {
-  name: stringCustomKey,
+  name: stringCustomKey.describe(
+    'A short name that could be used a key or variable for the step'
+  ),
   title: stringTitle.optional(),
   description: stringDescription.optional(),
   motivation: stringMotivation.optional(),
 };
-const lineShell = z.string().min(1).max(300);
-const advancedShell = z.object({
-  ...metadataStep,
-  onFailure: z.array(onShellCommandFinish).min(1).default(['exit']),
-  onSuccess: z.array(onShellCommandFinish).min(1).default(['trim']),
-  name: lineShell,
+const lineShell = z.string().min(1).max(300).describe('A line of shell script');
+const advancedShell = z
+  .object({
+    ...metadataStep,
+    onFailure: z
+      .array(onShellCommandFinish)
+      .min(1)
+      .default(['exit'])
+      .describe(
+        'List of flags to describe the default behavior in case of failure'
+      ),
+    onSuccess: z
+      .array(onShellCommandFinish)
+      .min(1)
+      .default(['trim'])
+      .describe(
+        'List of flags to describe the default behavior in case of success'
+      ),
+    name: lineShell,
 
-  if: varValue.optional(),
-  run: lineShell,
-});
+    if: varValue
+      .optional()
+      .describe(
+        'reference to JSON path that must be satisfied for the step to run'
+      ),
+    run: lineShell,
+  })
+  .describe('Configuration for the batch shell script');
 
-const commands = z.array(advancedShell).min(1).max(50);
+const commands = z
+  .array(advancedShell)
+  .min(1)
+  .max(50)
+  .describe('A list of batch shell scripts to run');
 
-const binary = z.object({
-  title: stringTitle,
-  description: stringDescription.optional(),
-  motivation: stringMotivation.optional(),
-  homepage: stringUrl.optional(),
-  shell: z.object({ run: lineShell, diagnosis: lineShell }),
-});
+const binary = z
+  .object({
+    title: stringTitle,
+    description: stringDescription.optional(),
+    motivation: stringMotivation.optional(),
+    homepage: stringUrl.optional(),
+    shell: z
+      .object({ run: lineShell, diagnosis: lineShell })
+      .describe('A few commands to provide quick diagnosis and support'),
+  })
+  .describe('Settings for a binary executable used in the developer workflow');
 const valuesLoopEach = z.string().min(1).max(300);
 
-const loopEach = z.object({
-  name: stringCustomKey,
-  values: valuesLoopEach,
-});
-const binaries = z.record(stringCustomKey, binary);
+const loopEach = z
+  .object({
+    name: stringCustomKey,
+    values: valuesLoopEach,
+  })
+  .describe('Configuration of every loop');
+const binaries = z
+  .record(stringCustomKey, binary)
+  .describe('A list of executables used in the developer workflow');
 
-const getPropertyStep = z.strictObject({
-  a: z.literal('get-property'),
-  ...metadataStep,
-  value: varValue,
-});
+const getPropertyStep = z
+  .strictObject({
+    a: z.literal('get-property'),
+    ...metadataStep,
+    value: varValue,
+  })
+  .describe('Get a property using a dot prop path');
 
 const onStringArraySuccess = z.enum(['sort', 'unique', 'filled', 'reverse']);
 
-const stringArrayFilterBy = z.strictObject({
-  if: z.enum([
-    'starts-with',
-    'ends-with',
-    'contains',
-    'equals',
-    'not starts-with',
-    'not ends-with',
-    'not contains',
-    'not equals',
-  ]),
-  anyOf: z.array(varValue).min(1).max(50),
-});
-const stringArrayStep = z.strictObject({
-  a: z.literal('string-array'),
-  ...metadataStep,
-  value: varValue,
-  onSuccess: onStringArraySuccess,
-  filters: z.array(stringArrayFilterBy).min(1).max(20).optional(),
-});
+const stringArrayFilterBy = z
+  .strictObject({
+    if: z
+      .enum([
+        'starts-with',
+        'ends-with',
+        'contains',
+        'equals',
+        'not starts-with',
+        'not ends-with',
+        'not contains',
+        'not equals',
+      ])
+      .describe('Filter criteria'),
+    anyOf: z
+      .array(varValue)
+      .min(1)
+      .max(50)
+      .describe('A list of references to match against'),
+  })
+  .describe('Filter an array of strings');
+const stringArrayStep = z
+  .strictObject({
+    a: z.literal('string-array'),
+    ...metadataStep,
+    value: varValue,
+    onSuccess: onStringArraySuccess,
+    filters: z
+      .array(stringArrayFilterBy)
+      .min(1)
+      .max(20)
+      .optional()
+      .describe('A list of filters on strings'),
+  })
+  .describe('Process on a list of strings');
 
-const concatArrayStep = z.strictObject({
-  a: z.literal('concat-array'),
-  ...metadataStep,
-  values: z.array(varValue).min(1).max(20),
-});
+const concatArrayStep = z
+  .strictObject({
+    a: z.literal('concat-array'),
+    ...metadataStep,
+    values: z.array(varValue).min(1).max(20),
+  })
+  .describe('Concatenate several arrays together');
 
-const splitStringStep = z.strictObject({
-  a: z.literal('split-string'),
-  ...metadataStep,
-  separator: z.string().min(1).max(80).default(' '),
-  value: varValue,
-});
+const splitStringStep = z
+  .strictObject({
+    a: z.literal('split-string'),
+    ...metadataStep,
+    separator: z
+      .string()
+      .min(1)
+      .max(80)
+      .default(' ')
+      .describe('A separator to split the string'),
+    value: varValue,
+  })
+  .describe('Split a string into multiple strings');
 
-const someTruthyArrayStep = z.strictObject({
-  a: z.literal('some-truthy'),
-  ...metadataStep,
+const someTruthyArrayStep = z
+  .strictObject({
+    a: z.literal('some-truthy'),
+    ...metadataStep,
 
-  values: z.array(varValue).min(1).max(20),
-});
+    values: z.array(varValue).min(1).max(20),
+  })
+  .describe('Return true if at least one of values is truthy');
 
-const someFalsyArrayStep = z.strictObject({
-  a: z.literal('some-falsy'),
-  ...metadataStep,
+const someFalsyArrayStep = z
+  .strictObject({
+    a: z.literal('some-falsy'),
+    ...metadataStep,
 
-  values: z.array(varValue).min(1).max(20),
-});
-const everyTruthyArrayStep = z.strictObject({
-  a: z.literal('every-truthy'),
-  ...metadataStep,
+    values: z.array(varValue).min(1).max(20),
+  })
+  .describe('Return true if at least one of values is falsy');
+const everyTruthyArrayStep = z
+  .strictObject({
+    a: z.literal('every-truthy'),
+    ...metadataStep,
 
-  values: z.array(varValue).min(1).max(20),
-});
-const everyFalsyArrayStep = z.strictObject({
-  a: z.literal('every-falsy'),
-  ...metadataStep,
+    values: z.array(varValue).min(1).max(20),
+  })
+  .describe('Return true if all the values are truthy');
 
-  values: z.array(varValue).min(1).max(20),
-});
+const everyFalsyArrayStep = z
+  .strictObject({
+    a: z.literal('every-falsy'),
+    ...metadataStep,
 
-const notStep = z.strictObject({
-  a: z.literal('not'),
-  ...metadataStep,
+    values: z.array(varValue).min(1).max(20),
+  })
+  .describe('Return true if all the values are falsy');
 
-  value: varValue,
-});
+const notStep = z
+  .strictObject({
+    a: z.literal('not'),
+    ...metadataStep,
 
-const rangeStep = z.strictObject({
-  a: z.literal('range'),
-  ...metadataStep,
+    value: varValue,
+  })
+  .describe('Return the opposite boolean value');
 
-  start: z.number().int().default(0),
-  end: z.number().int(),
-  step: z.number().int().default(1),
-});
-const invertObjectStep = z.strictObject({
-  a: z.literal('invert-object'),
-  ...metadataStep,
+const rangeStep = z
+  .strictObject({
+    a: z.literal('range'),
+    ...metadataStep,
 
-  value: varValue,
-});
-const anyBeforeStep = z.discriminatedUnion('a', [
-  getPropertyStep,
-  stringArrayStep,
-  concatArrayStep,
-  splitStringStep,
-  someTruthyArrayStep,
-  someFalsyArrayStep,
-  everyTruthyArrayStep,
-  everyFalsyArrayStep,
-  notStep,
-  rangeStep,
-  invertObjectStep,
-]);
+    start: z
+      .number()
+      .int()
+      .default(0)
+      .describe('The number to start the range with'),
+    end: z.number().int().describe('The number at the end of the range'),
+    step: z
+      .number()
+      .int()
+      .default(1)
+      .describe('A step to increment the range, usually 1'),
+  })
+  .describe('Generate a range of numbers');
+const invertObjectStep = z
+  .strictObject({
+    a: z.literal('invert-object'),
+    ...metadataStep,
 
-const batchStep = z.strictObject({
-  a: z.literal('batch'),
-  ...metadataStep,
-  before: z.array(anyBeforeStep).min(1).max(20).optional(),
-  if: varValue.optional(),
-  each: z.array(loopEach).min(1).max(3).optional(),
-  onFinish: z.array(onBatchStepFinish).min(1).optional(),
-  commands,
-});
+    value: varValue,
+  })
+  .describe('Invert keys and values into a new object');
+const anyBeforeStep = z
+  .discriminatedUnion('a', [
+    getPropertyStep,
+    stringArrayStep,
+    concatArrayStep,
+    splitStringStep,
+    someTruthyArrayStep,
+    someFalsyArrayStep,
+    everyTruthyArrayStep,
+    everyFalsyArrayStep,
+    notStep,
+    rangeStep,
+    invertObjectStep,
+  ])
+  .describe('An operation on the context');
+
+const batchStep = z
+  .strictObject({
+    a: z.literal('batch'),
+    ...metadataStep,
+    before: z
+      .array(anyBeforeStep)
+      .min(1)
+      .max(20)
+      .optional()
+      .describe('A list of operations to run before end'),
+    if: varValue
+      .optional()
+      .describe('A condition that must be satisfied to enable the batch'),
+    each: z
+      .array(loopEach)
+      .min(1)
+      .max(3)
+      .optional()
+      .describe('The same batch will be run multiple times for each loop'),
+    onFinish: z
+      .array(onBatchStepFinish)
+      .min(1)
+      .optional()
+      .describe('Expected behavior when the batch finishes'),
+    commands,
+  })
+  .describe('A batch of shell commands to run');
 const steps = z.array(batchStep).min(1);
-const parameter = z.object({
-  description: stringDescription,
-  flags: z.string().min(1).max(80),
-  default: z.string().min(1).max(300).optional(),
-});
-const task = z.object({
-  name: z.string().max(1).default(''),
-  title: stringTitle,
-  description: stringDescription.optional(),
-  motivation: stringMotivation.optional(),
-  parameters: z.record(stringCustomKey, parameter),
-  steps,
-  finally: steps.optional(),
-});
-const domain = z.object({
-  title: stringTitle,
-  description: stringDescription.optional(),
-  tasks: z.record(stringCustomKey, task),
-});
+const parameter = z
+  .object({
+    description: stringDescription,
+    flags: z.string().min(1).max(80),
+    default: z.string().min(1).max(300).optional(),
+  })
+  .describe('Settings for a task parameter');
+const task = z
+  .object({
+    name: z.string().max(1).default(''),
+    title: stringTitle,
+    description: stringDescription.optional(),
+    motivation: stringMotivation.optional(),
+    parameters: z.record(stringCustomKey, parameter),
+    steps,
+    finally: steps.optional(),
+  })
+  .describe('Settings for a task');
+const domain = z
+  .object({
+    title: stringTitle,
+    description: stringDescription.optional(),
+    tasks: z.record(stringCustomKey, task),
+  })
+  .describe('A domain for a list of tasks');
 export const schema = z
   .object({
     engine,
@@ -212,6 +324,7 @@ export const schema = z
     model: jsonishSchema,
     workflows: z.record(stringCustomKey, domain),
   })
+  .describe('Settings for a baldrick-broth file')
   .strict();
 
 const runtimeContext = z.object({
--- src/field-validation.ts
+++ src/field-validation.ts
@@ -6,23 +6,45 @@ export const stringCustomKey = z
   .string()
   .min(1)
   .max(30)
-  .regex(/[a-z][\d_a-z]+/);
+  .regex(/[a-z][\d_a-z]+/)
+  .describe('A short name that can used as variable');
 export const varValue = z
   .string()
   .min(1)
   .max(300)
-  .regex(/(([\d._a-z]+)|(\[\d+]))+/);
+  .regex(/(([\d._a-z]+)|(\[\d+]))+/).describe('A dot prop path');
 export const stringTitle = z
   .string()
   .trim()
   .min(1)
   .max(60)
-  .refine(isSingleLine, { message: 'title should be a single line' });
-export const stringDescription = z.string().trim().min(1).max(300);
-export const stringMotivation = z.string().trim().min(1).max(300);
-export const stringUrl = z.string().url().max(300);
-export const stringPath = z.string().max(300);
-export const stringPropPath = z.string().max(300);
+  .refine(isSingleLine, { message: 'title should be a single line' })
+  .describe('A short title that summarizes this section of script');
+export const stringDescription = z
+  .string()
+  .trim()
+  .min(1)
+  .max(300)
+  .describe('The main purpose of this section of script');
+export const stringMotivation = z
+  .string()
+  .trim()
+  .min(1)
+  .max(300)
+  .describe('The main reason why this section of script is needed');
+export const stringUrl = z
+  .string()
+  .url()
+  .max(300)
+  .describe('A https link to a webpage');
+export const stringPath = z
+  .string()
+  .max(300)
+  .describe('A relative path to a file');
+export const stringPropPath = z
+  .string()
+  .max(300)
+  .describe('A dot prop path');
 
 export const safeParseField = (
   name: 'title' | 'filename' | string,
