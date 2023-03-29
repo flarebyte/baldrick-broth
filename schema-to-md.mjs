3b40a0e98df31302ce478e18c581b6c1781b6d24
Olivier Huin <olih@users.noreply.github.com>
2023-03-29 19:33:17 +0100

    Adds icone

--- schema-to-md.mjs
+++ schema-to-md.mjs
@@ -4,34 +4,37 @@ const fs = require('node:fs');
 
 const schemaPath = 'spec/snapshots/build-model/get-schema--schema.json';
 const markdownPath = 'SCHEMA.md';
+const title = 'baldrick-broth'
 
 const schema = JSON.parse(fs.readFileSync(schemaPath, 'utf8'));
 
 const pad = (count) => ' '.repeat(count * 2);
 
-function getProperties(obj, level) {
-  let properties = '';
+function getProperties(obj, level, kind) {
+  let content = '';
   for (const [key, value] of Object.entries(obj)) {
     console.log(key);
     const description = value.description || '_';
-    properties += `${pad(level)}- ${key}: ${description}\n`;
+    const ref = value['$ref'] || ''
+    content += `${pad(level)}- ${kind} ${key}: ${ref}${description}\n`;
     if (value.properties) {
-      properties += getProperties(value.properties, level + 1);
+      content += getProperties(value.properties, level + 1, '◆');
     }
 
     if (value.additionalProperties) {
-      properties += getProperties(
+      content += getProperties(
         value.additionalProperties.properties,
-        level + 1
+        level + 1,
+        '◇'
       );
     }
   }
 
-  return properties;
+  return content;
 }
 
-const markdownProps = getProperties(schema.definitions, 0);
-const markdown = `# Schema
+const markdownProps = getProperties(schema.definitions, 0, '');
+const markdown = `# Schema for ${title}
 
 ${markdownProps}
 `;
